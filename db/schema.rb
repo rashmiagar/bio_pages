1d43a50863efd09cf7243a9935421f953253287f
Rashmi Agarwal <rashmiagar@gmail.com>
2015-08-09 12:50:37 +0530

    migration added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803165220) do
+ActiveRecord::Schema.define(version: 20150809063951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20150803165220) do
     t.string "name"
     t.string "email"
     t.string "designation"
+    t.string "uid"
   end
 
 end
