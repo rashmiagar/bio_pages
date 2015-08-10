0c7acfcfa3e0f095ce5d8e89b3c237ac0707e963
Rashmi Agarwal <rashmiagar@gmail.com>
2015-08-10 18:00:29 +0530

    Authentication and view changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+
   def landing
+  	if signed_in?
+  	  redirect_to show_bio_pic_page_path(current_user)
+  	end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
   def create
     @authorization = User.find_by_uid(auth_hash["uid"])
     if @authorization
-      #sign_in @authorization, :event => :authentication
-      render :text => "Welcome back #{@authorization.name}! You have already signed up."  
+      sign_in @authorization
+      redirect_to show_bio_pic_page_path(current_user), :notice => "Welcome back #{@authorization.name}! You have already signed up."  
     else
       user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"], :uid => auth_hash["uid"]
       user.save
-      redirect_to root_url :notice => "Hi #{user.name}! You've signed up."
+      redirect_to show_bio_pic_page_path(current_user), :notice => "Hi #{user.name}! You've signed up."
     end
   end
 
