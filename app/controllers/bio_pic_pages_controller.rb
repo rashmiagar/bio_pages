f0832993788612dd3f3121df63e5ba92e178fd53
Rashmi Agarwal <rashmiagar@gmail.com>
2015-08-28 18:41:39 +0530

    fixed some issue in saving skills

--- app/controllers/bio_pic_pages_controller.rb
+++ app/controllers/bio_pic_pages_controller.rb
@@ -24,11 +24,10 @@ class BioPicPagesController < ApplicationController
   	begin
       skills.each do |key, value|
         name = value["name"]
-        skill_db_object = Skill.find_by_name(name)
-        
+        skill_db_object = Skill.find_by_name(name.try(:downcase))
         if !skill_db_object.present? && name.present?
          # binding.pry
-          skill = Skill.create(name: name, :category_id => value["category_id"].to_i)
+          skill = Skill.create(name: name.downcase, :category_id => value["category_id"].to_i)
           @user.user_skills.create!(:skill_id => skill.id, :mastered => value["mastered"], :description => value["description"])
         elsif skill_db_object.present? && name.present?
           @user.user_skills.create!(:skill_id => skill_db_object.id, :mastered => value["mastered"], :description => value["description"])
